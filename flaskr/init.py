e2639592fc503b19476841c7f32a1772544a66a1
boruchk <boruk770@gmail.com>
2025-06-13 16:05:29 -0400

    .

--- flaskr/init.py
+++ flaskr/init.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 #Configure MySQL
 conn = pymysql.connect(host='localhost',
-		       port = 3306,
+		       						 port = 3306,
                        user='root',
                        password='',
                        db='Project',
